d670621c2ad9cddb3e73988470b24cbcf3a4f14d
Kuba Sukowski <kubasulek2@gmail.com>
2019-01-16 18:55:38 +0100

    basic fetch function create

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,19 @@
-$( () => {
+$(() => {
 
+  const basicFetch = ()=>{
+    const appId = 'fd3ea657';
+    const appKey ='a61ca3c11d3b2ec930779e11cfe06c85';
+
+    fetch(`https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}
+`,{
+      mode: 'cors',
+      redirect: 'follow',
+      headers: {
+        'Accept-Encoding': 'gzip',
+      }
+    }).then(resp => resp.json())
+      .then(data =>{
+        console.log(data);
+      })
+  }
 });
