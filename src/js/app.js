7bc3e0408e6b2b70655947ae32d19cc388cde00c
Kuba Sukowski <kubasulek2@gmail.com>
2019-01-17 15:28:03 +0100

    bootstrap properly added

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,7 @@ $(() => {
     const appId = 'fd3ea657';
     const appKey ='a61ca3c11d3b2ec930779e11cfe06c85';
 
-    fetch(`https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}
-`,{
+    fetch(`https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}`,{
       mode: 'cors',
       redirect: 'follow',
       headers: {
@@ -15,5 +14,6 @@ $(() => {
       .then(data =>{
         console.log(data);
       })
-  }
+  };
+  //basicFetch()
 });
